5f145e0ec3d4694add572dec3f74ed4754238f30
Teo Espero <38012835+teoespero@users.noreply.github.com>
2023-02-20 22:26:19 -0800

    Add files via upload

---
+++ DisplayFiveMessages.java
@@ -0,0 +1,20 @@
+//////////////////////////////////////////////////////////////////////////////
+//  WelcomeToSWE
+//  DisplayFiveMessages.java
+//  Author: Teo Espero
+//          BS Software Engineering
+//          BS Cloud and Systems Administration
+//          Western Governors University
+//  Description:
+//          Write a program that displays Welcome to Java 5x,
+//  Date written: 02/20/2023
+//////////////////////////////////////////////////////////////////////////////
+public class DisplayFiveMessages {
+    public static void main (String[] args){
+        int lineCtr;
+        for(lineCtr = 0; lineCtr < 5; lineCtr++){
+            System.out.println(lineCtr + ": Welcome to Java!");
+        }
+    }
+}
+//////////////////////////////////////////////////////////////////////////////
