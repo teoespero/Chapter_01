be83032beb86c6bbd6fcf4c0cd584b8cf64fa0f9
Teo Espero <38012835+teoespero@users.noreply.github.com>
2023-02-20 23:03:33 -0800

    Add files via upload

---
+++ ComputeExpressions.java
@@ -0,0 +1,23 @@
+//////////////////////////////////////////////////////////////////////////////
+//  WelcomeToSWE
+//  ComputeExpressions.java
+//  Author: Teo Espero
+//          BS Software Engineering
+//          BS Cloud and Systems Administration
+//          Western Governors University
+//  Description:
+//          Write a program that displays Welcome to Java 5x,
+//  Date written: 02/20/2023
+//////////////////////////////////////////////////////////////////////////////
+public class ComputeExpressions {
+    public static void main(String[] args){
+        double answer;
+
+        answer = (9.5 * 4.5) - (2.5 * 3.0)/(45.5 - 3.5);
+
+        System.out.println(" 9.5 X 4.5 - 2.5 X 3");
+        System.out.println("--------------------- = " + answer);
+        System.out.println("     45.5 - 3.5");
+    }
+}
+//////////////////////////////////////////////////////////////////////////////
