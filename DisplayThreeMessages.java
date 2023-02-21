b84c27e38b3716ca76e7a55059586235c943dc9e
Teo Espero <38012835+teoespero@users.noreply.github.com>
2023-02-20 22:15:12 -0800

    Add files via upload

---
+++ DisplayThreeMessages.java
@@ -0,0 +1,20 @@
+//////////////////////////////////////////////////////////////////////////////
+//  WelcomeToSWE
+//  Author: Teo Espero
+//          BS Software Engineering
+//          BS Cloud and Systems Administration
+//          Western Governors University
+//  Description:
+//          Write a program that displays Welcome to Java,
+//          Welcome to Computer Science,
+//          and Programming is fun.
+//  Date written: 02/20/2023
+//////////////////////////////////////////////////////////////////////////////
+public class DisplayThreeMessages {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java!");
+        System.out.println("Welcome to Software Engineering");
+        System.out.println("Programming is Fun!");
+    }
+}
+//////////////////////////////////////////////////////////////////////////////
