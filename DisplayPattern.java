51db442ed6e907bf534c689b14879db859643f8d
Teo Espero <38012835+teoespero@users.noreply.github.com>
2023-02-20 22:39:29 -0800

    Add files via upload

---
+++ DisplayPattern.java
@@ -0,0 +1,20 @@
+//////////////////////////////////////////////////////////////////////////////
+//  WelcomeToSWE
+//  DisplayPattern.java
+//  Author: Teo Espero
+//          BS Software Engineering
+//          BS Cloud and Systems Administration
+//          Western Governors University
+//  Description:
+//          Write a program that displays a pattern,
+//  Date written: 02/20/2023
+//////////////////////////////////////////////////////////////////////////////
+public class DisplayPattern {
+    public static void main(String[] args){
+        System.out.println("     J          A          V          V          A");
+        System.out.println("     J        A    A         V      V          A    A");
+        System.out.println("     J       A A A A A        V    V          A A A A A");
+        System.out.println("J    J       A       A         V  V           A       A");
+        System.out.println("  J J        A       A           A            A       A ");
+    }
+}
